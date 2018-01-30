e933d4059ac3f5af3034b5f13efe611278aee677
Matthew Montoya <msmontoya2@miners.utep.edu>
2018-01-29 19:54:08 -0700

    issue remains with checking the inner boxes

--- HW_1_Absence/src/edu/utep/cs/cs3331/sudoku/Board.java
+++ HW_1_Absence/src/edu/utep/cs/cs3331/sudoku/Board.java
@@ -12,7 +12,7 @@ package edu.utep.cs.cs3331.sudoku;
 import java.util.Random;
 
 public class Board extends ConsoleUI {
-	private static int[][] array;
+	private static int[][] backendBoard;
 	private static ConsoleUI ui = new ConsoleUI();
 	private int size;
 	private static boolean bool;
@@ -22,28 +22,65 @@ public class Board extends ConsoleUI {
 	}
 
 	public Board(int size) {	//creates the board to be of size n
-		int[][] backendBoard = new int[size][size];
+		backendBoard = new int[size][size];
 		for(int i=0; i<backendBoard.length; i++) {
 			for(int j=0; j<backendBoard[i].length; j++) {
-				backendBoard[i][j] = -1;
+				backendBoard[i][j] = 0;
 			}
 		}
-	}
-		
-	
-	public static void editBoard(int size, int x, int y, int value) {
-		array[x][y] = value;
-		ui.printBoard(size, array);	
+		ui.printBoard(size, backendBoard);
 	}
 
+
+	public static void editBoard(int size, int x, int y, int value) {
+		backendBoard[x-1][y-1] = value;
+
+		boolean isSolved = true;
+		for(int i=0; i<backendBoard.length; i++) {
+			for(int j=0; j<backendBoard[i].length; j++) {
+				if(backendBoard[i][j] == 0) {
+					isSolved=false;
+					setIsSolved(isSolved);
+					break;
+				}
+			}
+		}
+		ui.printBoard(size, backendBoard);	
+	}
+
+	public static boolean rowChecker(int x_coordinate, int value) {
+		for (int i=0; i<backendBoard.length; i++)
+			if (backendBoard[x_coordinate][i] == value)
+				return false;
+		return true;
+	}
+
+	public static boolean columnChecker(int y_coordinate, int value) {
+		for (int i=0; i<backendBoard.length; i++)
+			if (backendBoard[i][y_coordinate] == value)
+				return false;
+		return true;
+	}
 	
+	public static boolean boxChecker(int x_coordinate, int y_coordinate, int value) {
+        int[] box = {
+        		x_coordinate-(x_coordinate%backendBoard.length), y_coordinate-(y_coordinate%backendBoard.length)
+        		};
+
+        for(int i=box[0]; i<=(box[0]+2); i++)
+            for(int j=box[1]; j<=(box[1]+2); j++)
+                if(backendBoard[i][j] == value)
+                    return false;
+        return true;
+    }
+
 	//FIXME: 
 	private static Random rand = new Random();
 
 	public static void setIsSolved(boolean sentinal) {
 		bool = sentinal;
 	}
-	
+
 	public static boolean isSolved() {
 		return bool;
 		//return rand.nextBoolean();
--- HW_1_Absence/src/edu/utep/cs/cs3331/sudoku/ConsoleUI.java
+++ HW_1_Absence/src/edu/utep/cs/cs3331/sudoku/ConsoleUI.java
@@ -55,128 +55,105 @@ public class ConsoleUI {
 	}
 
 	public void playGame(Board board, int size) {
-		//STEP 1: Print the empty bad board
-		printBoard(size);
+		while(!board.isSolved()) {
+			//STEP 1: Print the empty bad board
+			//printBoard(size);
 
-		//STEP 2: Have user select x,y and value, then add value (pass through real values to backendView)
-		input=new Scanner(System.in);
-		int x_coordinate=0, y_coordinate=0, value=0;
+			//STEP 2: Have user select x,y and value, then add value (pass through real values to backendView)
+			input=new Scanner(System.in);
+			int x_coordinate=0, y_coordinate=0, value=0;
 
-		/********************************************************************
-		 ************************GET THE X-COORDINATE************************
-		 ********************************************************************/		
-		out.println("Select the coordinate you'd like to edit or enter '0 or a negative number' to terminate.\nLet's begin with the X-Value:");
-		x_coordinate = input.nextInt();
-		if(x_coordinate < 1) {
-			showExitMessage();
-			Board.setIsSolved(true);
-		}
-		while(x_coordinate > size) {
-			out.println("Invalid input.\nPlease enter numbers between 1 & "+size+" or enter '0' or a negative number to terminate.");
+			/********************************************************************
+			 ************************GET THE X-COORDINATE************************
+			 ********************************************************************/		
+			out.println("Select the coordinate you'd like to edit or enter '0 or a negative number' to terminate.\nLet's begin with the row (beginning from the top):");
 			x_coordinate = input.nextInt();
-			if(x_coordinate < 1)
+			if(x_coordinate < 1) {
 				showExitMessage();
-			if(x_coordinate>0 && x_coordinate<=size) {
+				Board.setIsSolved(true);
 				break;
 			}
-
-			if(x_coordinate > size || x_coordinate < 0) {
-				out.println("Invalid input.\nPlease enter numbers between 1 & "+size+".");
-				playGame(board, size);
-			}
-		}
-
-		/********************************************************************
-		 ************************GET THE Y-COORDINATE************************
-		 ********************************************************************/
-		if(!Board.isSolved()) {
-			out.println("Great! You chose "+x_coordinate+". Now, for the Y-Value. Select the coordinate you'd like to edit or enter '0' or a negative number to terminate.");
-			y_coordinate = input.nextInt();
-			if(y_coordinate <1)
-				showExitMessage();
-			while(y_coordinate > size) {		//if an invalid number is entered
+			while(x_coordinate > size) {
 				out.println("Invalid input.\nPlease enter numbers between 1 & "+size+" or enter '0' or a negative number to terminate.");
+				x_coordinate = input.nextInt();
+				if(x_coordinate < 1)
+					showExitMessage();
+				if(x_coordinate>0 && x_coordinate<=size) {
+					break;
+				}
+
+				if(x_coordinate > size || x_coordinate < 0) {
+					out.println("Invalid input.\nPlease enter numbers between 1 & "+size+".");
+					playGame(board, size);
+				}
+			}
+
+			/********************************************************************
+			 ************************GET THE Y-COORDINATE************************
+			 ********************************************************************/
+			if(!Board.isSolved()) {
+				out.println("Great! You chose "+x_coordinate+". Now, for the column (beginning from the top). Select the coordinate you'd like to edit or enter '0' or a negative number to terminate.");
 				y_coordinate = input.nextInt();
-				if(y_coordinate < 1)
+				if(y_coordinate <1) {
 					showExitMessage();
-				if(y_coordinate>0 && y_coordinate<=size)
+					Board.setIsSolved(true);
 					break;
-			}
-		}
-
-		/********************************************************************
-		 ********************GET THE COORDINATE VALUE************************
-		 ********************************************************************/		
-		if(!Board.isSolved()) {
-			out.println("Enter the value you wish to put in or enter '0' or a negative number to terminate.");
-			value = input.nextInt();
-			if(value <1) {
-				showExitMessage();
-				//board.isSolved(true);
+				}
+				while(y_coordinate > size) {		//if an invalid number is entered
+					out.println("Invalid input.\nPlease enter numbers between 1 & "+size+" or enter '0' or a negative number to terminate.");
+					y_coordinate = input.nextInt();
+					if(y_coordinate < 1)
+						showExitMessage();
+					if(y_coordinate>0 && y_coordinate<=size)
+						break;
+				}
 			}
 
-			while(value > size) {		//if an invalid number is entered
-				out.println("Invalid input.\nPlease enter numbers between 1 & "+size+" or enter '0' or a negative number to terminate.");
+			/********************************************************************
+			 ********************GET THE COORDINATE VALUE************************
+			 ********************************************************************/		
+			if(!Board.isSolved()) {
+				out.println("Enter the value you wish to put in or enter '0' or a negative number to terminate.");
 				value = input.nextInt();
-				if(value < 1)
+				if(value <1) {
 					showExitMessage();
-				if(value>0 && value<=size)
+					Board.setIsSolved(true);
 					break;
+				}
+
+				while(value > size) {		//if an invalid number is entered
+					out.println("Invalid input.\nPlease enter numbers between 1 & "+size+" or enter '0' or a negative number to terminate.");
+					value = input.nextInt();
+					if(value < 1)
+						showExitMessage();
+					if(value>0 && value<=size)
+						break;
+				}
+			}
+			/********************************************************************
+			 *********************EDIT + PRINT THE BOARD*************************
+			 ********************************************************************/	
+			if(!Board.isSolved()) {
+				
+				boolean goesInRow = Board.rowChecker(x_coordinate, value);
+				boolean goesInColumn = Board.columnChecker(y_coordinate, value);
+				boolean goesInBox =  Board.boxChecker(x_coordinate, y_coordinate, value);
+				
+				if(goesInRow && goesInColumn && goesInBox)
+							Board.editBoard(size, x_coordinate, y_coordinate, value);
+				else {
+					if(!goesInRow)
+						out.println("Your number cannot go in this row.");
+					if(!goesInColumn)
+						out.println("Your number cannot go in this column.");
+					if(!goesInBox)
+						out.println("Your number cannot go in this box.");
+				}
 			}
 		}
-		/********************************************************************
-		 ***************** ****EDIT + PRINT THE BOARD************************
-		 ********************************************************************/	
-		Board.editBoard(size, x_coordinate, y_coordinate, value);
-
-
-		/********************************************************************
-		 *************************PRINT THE BOARD****************************
-		 ********************************************************************/	
 	}
-
-	//PLAYING THE GAME ENDS	
-
-
-	/********************************************************************
-	 ********************PRINT INITAL EMPTY BOARD************************
-	 ********************************************************************/
-	public void printBoard(int size){
-		if(size==4) {
-			out.println("+---+---+---+---+");
-			out.println("|   |   !   |   |");
-			out.println("+---+---*---+---+");
-			out.println("|   |   !   |   |");
-			out.println("+===+===*===+===+");
-			out.println("|   |   !   |   |");
-			out.println("+---+---*---+---+");
-			out.println("|   |   !   |   |");
-			out.println("+---+---+---+---+");
-		}
-		else if(size==9) {
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+===+===+===*===+===+===*===+===+===+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+===+===+===*===+===+===*===+===+===+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-			out.println("|   |   |   !   |   |   !   |   |   |");
-			out.println("+---+---+---*---+---+---*---+---+---+");
-		}
-
-	}
-
+	
+	
 
 	/**
 	 * printBoard call that will be made every time the user inputs a value in the board
@@ -200,40 +177,40 @@ public class ConsoleUI {
 		if(size==9) {
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[0][0]+" | "+array[0][1]+" | "+array[0][2]+" ! "
-					+ ""+array[0][3]+" |"+array[0][4]+" | "+array[0][5]+" ! "
-					+ ""+array[0][6]+" | "+array[0][7]+" |"+array[0][8]+" |");
+					+ ""+array[0][3]+" | "+array[0][4]+" | "+array[0][5]+" ! "
+					+ ""+array[0][6]+" | "+array[0][7]+" | "+array[0][8]+" |");
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[1][0]+" | "+array[1][1]+" | "+array[1][2]+" ! "
-					+ ""+array[1][3]+" |"+array[1][4]+" | "+array[1][5]+" ! "
-					+ ""+array[1][6]+" | "+array[1][7]+" |"+array[1][8]+" |");			
+					+ ""+array[1][3]+" | "+array[1][4]+" | "+array[1][5]+" ! "
+					+ ""+array[1][6]+" | "+array[1][7]+" | "+array[1][8]+" |");			
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[2][0]+" | "+array[2][1]+" | "+array[2][2]+" ! "
-					+ ""+array[2][3]+" |"+array[2][4]+" | "+array[2][5]+" ! "
-					+ ""+array[2][6]+" | "+array[2][7]+" |"+array[2][8]+" |");
+					+ ""+array[2][3]+" | "+array[2][4]+" | "+array[2][5]+" ! "
+					+ ""+array[2][6]+" | "+array[2][7]+" | "+array[2][8]+" |");
 			out.println("+===+===+===*===+===+===*===+===+===+");
 			out.println("| "+array[3][0]+" | "+array[3][1]+" | "+array[3][2]+" ! "
-					+ ""+array[3][3]+" |"+array[3][4]+" | "+array[3][5]+" ! "
-					+ ""+array[3][6]+" | "+array[3][7]+" |"+array[3][8]+" |");			
+					+ ""+array[3][3]+" | "+array[3][4]+" | "+array[3][5]+" ! "
+					+ ""+array[3][6]+" | "+array[3][7]+" | "+array[3][8]+" |");			
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[4][0]+" | "+array[4][1]+" | "+array[4][2]+" ! "
-					+ ""+array[4][3]+" |"+array[4][4]+" | "+array[4][5]+" ! "
-					+ ""+array[4][6]+" | "+array[4][7]+" |"+array[4][8]+" |");
+					+ ""+array[4][3]+" | "+array[4][4]+" | "+array[4][5]+" ! "
+					+ ""+array[4][6]+" | "+array[4][7]+" | "+array[4][8]+" |");
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[5][0]+" | "+array[5][1]+" | "+array[5][2]+" ! "
-					+ ""+array[5][3]+" |"+array[5][4]+" | "+array[5][5]+" ! "
-					+ ""+array[5][6]+" | "+array[5][7]+" |"+array[5][8]+" |");			
+					+ ""+array[5][3]+" | "+array[5][4]+" | "+array[5][5]+" ! "
+					+ ""+array[5][6]+" | "+array[5][7]+" | "+array[5][8]+" |");			
 			out.println("+===+===+===*===+===+===*===+===+===+");
 			out.println("| "+array[6][0]+" | "+array[6][1]+" | "+array[6][2]+" ! "
-					+ ""+array[6][3]+" |"+array[6][4]+" | "+array[6][5]+" ! "
-					+ ""+array[6][6]+" | "+array[6][7]+" |"+array[6][8]+" |");			
+					+ ""+array[6][3]+" | "+array[6][4]+" | "+array[6][5]+" ! "
+					+ ""+array[6][6]+" | "+array[6][7]+" | "+array[6][8]+" |");			
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[7][0]+" | "+array[7][1]+" | "+array[7][2]+" ! "
-					+ ""+array[7][3]+" |"+array[7][4]+" | "+array[7][5]+" ! "
-					+ ""+array[7][6]+" | "+array[7][7]+" |"+array[7][8]+" |");			
+					+ ""+array[7][3]+" | "+array[7][4]+" | "+array[7][5]+" ! "
+					+ ""+array[7][6]+" | "+array[7][7]+" | "+array[7][8]+" |");			
 			out.println("+---+---+---*---+---+---*---+---+---+");
 			out.println("| "+array[8][0]+" | "+array[8][1]+" | "+array[8][2]+" ! "
-					+ ""+array[8][3]+" |"+array[8][4]+" | "+array[8][5]+" ! "
-					+ ""+array[8][6]+" | "+array[8][7]+" |"+array[8][8]+" |");			
+					+ ""+array[8][3]+" | "+array[8][4]+" | "+array[8][5]+" ! "
+					+ ""+array[8][6]+" | "+array[8][7]+" | "+array[8][8]+" |");			
 			out.println("+---+---+---*---+---+---*---+---+---+");
 		}
 
